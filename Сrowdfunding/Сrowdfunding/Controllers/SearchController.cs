0c1e150323392e1bae702300878d7c86f6108cb9
n.malashko <n.malashko@itransition.com>
2021-12-02 02:48:15 +0300

    added search

--- Сrowdfunding/Сrowdfunding/Controllers/SearchController.cs
+++ Сrowdfunding/Сrowdfunding/Controllers/SearchController.cs
@@ -1,16 +1,47 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Сrowdfunding.Data;
+using Сrowdfunding.Models.ViewModels;
 
 namespace Сrowdfunding.Controllers
 {
     public class SearchController : Controller
     {
+        private readonly ILogger<SearchController> _logger;
+        private ApplicationDbContext _context;
+        private UserManager<IdentityUser> _userManager;
+
+        public SearchController(ILogger<SearchController> logger, ApplicationDbContext context, UserManager<IdentityUser> userManager)
+        {
+            _logger = logger;
+            _context = context;
+            _userManager = userManager;
+        }
+
         public IActionResult Index()
         {
             return View();
+        }
+
+        public IActionResult Search(string searchString)
+        {
+            var campaigns = _context.Campaigns.Where(c => c.Name.Contains(searchString)
+                                                    || c.ShortDescription.Contains(searchString)
+                                                    || c.Story.Contains(searchString)
+                                                    || c.Comments.Where(com => com.Content.Contains(searchString)).Any()
+                                                    || c.News.Where(n => n.NewsContent.Contains(searchString)).Any()).ToList();
+            var searchViewModel = new SearchViewModel
+            {
+                Campaigns = campaigns,
+                SearchString = searchString
+            };
+
+            return View("Index", searchViewModel);
         }
     }
 }
---
+++ Сrowdfunding/Сrowdfunding/Models/ViewModels/SearchViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Сrowdfunding.Models.ViewModels
+{
+    public class SearchViewModel
+    {
+        public List<Campaign> Campaigns { get; set; }
+
+        public string SearchString { get; set; }
+    }
+}
